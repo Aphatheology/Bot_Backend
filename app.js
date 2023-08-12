bda6fbba0027d5fb6c1cfda244570c95ea1074fe
Aphatheology <mustapha.abdulkareem@saritay.com>
2023-08-12 12:33:30 +0100

    improve validation and minor fix

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const config = require("./src/config/config");
 const morgan = require("./src/config/morgan");
-const passport = require("passport");
 const { errorConverter, errorHandler } = require("./src/middleware/error");
+
 const paymentRoute = require("./src/payments/payment.route");
 const flightRoute = require("./src/flights/flight.route");
-// require("./src/config/passport");
 const bookingRoute = require("./src/bookings/booking.route");
-// require("./src/config/passport");
 
 const app = express();
 
@@ -16,7 +14,6 @@ if (config.env !== "test") {
     app.use(morgan.errorHandler);
 }
 app.use(express.json());
-// app.use(passport.initialize());
 
 app.use("/transact", paymentRoute);
 app.use("/flight", flightRoute);
@@ -26,7 +23,7 @@ app.get("/", (req, res) => {
     res.send({ message: "Welcome to Flight Bot Backend" });
 });
 app.use("*", (req, res) => {
-    res.send({ message: "Route Not found" });
+    res.statusCode(404).send({ message: "Route Not found" });
 });
 
 app.use(errorConverter);
--- src/flights/flight.validation.js
+++ src/flights/flight.validation.js
@@ -1,35 +1,22 @@
 const Joi = require('joi');
+const moment = require('moment');
 
-const createSingleFlight = {
-    body: Joi.object().keys({
+const getAvailableFlights = {
+    query: Joi.object().keys({
         departure: Joi.string().required(),
         destination: Joi.string().required(),
-        date: Joi.date().iso().greater('now').required(),
-        price: Joi.number().required(),
+        date: Joi.date()
+            .iso()
+            .greater(moment().subtract(1, 'day'))
+            .raw()
+            .required()
+            .messages({
+                'date.format': 'Date must be in the format yyyy-mm-dd, e.g. 2023-08-20',
+                'date.greater': 'Date can\'t be less than today'
+            }),
     }),
 };
 
-const createMultipleFlights = {
-    body: Joi.array().items(
-        Joi.object({
-            departure: Joi.string().required(),
-            destination: Joi.string().required(),
-            date: Joi.date().iso().greater('now').required(),
-            price: Joi.number().required(),
-        })
-    ),
-};
-
-const getAvailableFlights = {
-  query: Joi.object().keys({
-      departure: Joi.string().required(),
-      destination: Joi.string().required(),
-      date: Joi.date().iso().greater('now').required(),
-  }),
-};
-
 module.exports = {
-    createSingleFlight,
-    createMultipleFlights,
-    getAvailableFlights
+    getAvailableFlights,
 };
